c79b90eb04ea2a6b4fb167c15e60974449f4de0b
BiteDasher <48867887+BiteDasher@users.noreply.github.com>
2020-07-04 14:12:35 +0300

    Небольшие оптимизации

--- bzu-gmb-launcher.sh
+++ bzu-gmb-launcher.sh
@@ -1,82 +1,39 @@
 #!/bin/bash
 
 # определение папки где находиться скрипт и версию скрипта
-script_dir0=$(cd $(dirname "$0") && pwd);
-export script_dir="${script_dir0}"
-version0=`cat "${script_dir}/config/name_version"`
-export version="${version0}"
+export script_dir="$(cd $(dirname "$0") && pwd)"
+export version="$(cat "${script_dir}/config/name_version")"
 
 # запрос пароля супер пользователя, который дальше будет поставляться где требуется в качестве глобальной переменной, до конца работы скрипта
-if pass_user0=$(zenity --entry --title="Для работы скрипта ${version} требуется пароль root" --text="Введите пароль:" \
+pass_user=$(zenity --entry --title="Для работы скрипта ${version} требуется пароль root" --text="Введите пароль:" \
  --entry-text="пароль" --hide-text --width=460 --height=128)
-then
-export pass_user=${pass_user0}
-#echo "pass:${pass_user}"
-else
+if [ -z "${pass_user} " ]; then
   zenity --error --text="Пароль не введён"
-exit 0
+exit 1
 fi
-
-#проверка установлен или нет yad и другое необходимое ПО для bzu-gmb
-dpkg -s yad | grep installed > /dev/null || echo 'no installing yad :(' | echo "$pass_user" | sudo -S apt install -f -y yad
-YadStatus=`dpkg -s yad | grep installed`
-echo "YAD" $YadStatus
-
-#проверяем установлена утилита inxi - информация о низкоуровневом ПО и железе
-dpkg -s inxi | grep installed > /dev/null || echo 'no install inxi :(' | echo "$pass_user" | sudo -S apt install -f -y inxi
-inxistatus=`dpkg -s inxi | grep installed`
-echo "INXI" $inxistatus
-
-#проверяем установлена утилита meson - она необходима для сборки многих программ из исходников
-dpkg -s meson | grep installed > /dev/null || echo 'no install meson :(' | echo "$pass_user" | sudo -S apt install -f -y meson
-inxistatus=`dpkg -s inxi | grep installed`
-echo "meson" $inxistatus
-
-#проверяем установлена утилита ninja-build - она необходима для сборки многих программ из исходников
-dpkg -s ninja-build | grep installed > /dev/null || echo 'no install ninja-build :(' | echo "$pass_user" | sudo -S apt install -f -y ninja-build
-inxistatus=`dpkg -s ninja-build | grep installed`
-echo "ninja-build" $inxistatus
-
-#проверяем установлена утилита p7zip-rar - она необходима для установки многих программ
-dpkg -s p7zip-rar | grep installed > /dev/null || echo 'no install p7zip-rar :(' | echo "$pass_user" | sudo -S apt install -f -y p7zip-rar rar unrar unace arj
-inxistatus=`dpkg -s ninja-build | grep installed`
-echo "p7zip-rar" $inxistatus
-
-#проверяем установлена утилита python-tk - она необходима для установки многих программ
-dpkg -s python-tk | grep installed > /dev/null || echo 'no install p7zip-rar :(' | echo "$pass_user" | sudo -S apt install -f -y python-tk
-inxistatus=`dpkg -s python-tk | grep installed`;echo "python-tk" $inxistatus
-
-#проверяем установлена утилита xosd-bin - она необходима для работы многих программ
-dpkg -s xosd-bin | grep installed > /dev/null || echo 'no install xosd-bin :(' | echo "$pass_user" | sudo -S apt install -f -y xosd-bin
-inxistatus=`dpkg -s xosd-bin | grep installed`;echo "xosd-bin" $inxistatus
+checkpkg() {
+apt-mark showmanual | grep -x $1 &>/dev/null || { echo "$1 is not installed :(" ; echo "${pass_user}" | sudo -S apt install -f -y ${@:1} ; }
+dpkgstatus="$(dpkg -s $1 | grep installed)"; echo "$1" "$dpkgstatus"
+}
+checkpkg yad # аналог zenity
+checkpkg inxi # низкоуровневое ПО для вывода информации о железе
+checkpkg meson # система сборки
+checkpkg ninja-build # система сборки
+checkpkg p7zip-rar rar unrar unace arj # архиватор
+checkpkg python-tk # python toolkit
+checkpkg xosd-bin # X On-Screen Display library 
 
 # проверка что за система запустила скрипт
-linuxos=`grep '^PRETTY_NAME' /etc/os-release`
+eval $(source /etc/os-release; echo linuxos="$PRETTY_NAME";)
 
 #загружаем список операционных систем из файла в массив
-readarray -t linuxos_list < "${script_dir}/config/list-os"
+#readarray -t linuxos_list < "${script_dir}/config/list-os"
 
-#создаем цикл где будет проверяться согласно данных из файла в какой системе запущен скрипт
-linuxos_num=${#linuxos_list[@]}
-#задаем переменные, что бы отработал заглушка на неправильную ОС и очищаем переменную для цикла на всякий случай
-linuxos_status=0
-i=0
-
-while [ $i -lt $linuxos_num ]
-do
-#проверяем на совпадение списка систем из файла и системы в которой запущен скрипт
-echo $linuxos | grep "${linuxos_list[$i]}" > /dev/null
-if [ $? = 0 ];then
-#если есть совпадение формируем команду запуска главного модуля для нужной системы
-start0="bash ${script_dir}/bzu-gmb-${linuxos_list[$i]}-beta4.sh"
-echo "Linux OS:${linuxos_list[$i]}"
-export linuxos_version=${linuxos_list[$i]}
-#присваиваем значение переменной что бы заглушка на не поддерживаемую ОС отключилась, при отмене установки
-linuxos_status=$i
-#echo "start0:$start0"
-fi
-i=$(($i + 1))
-done
+# узнаём текущую версию OS
+if [ "$(cat ${script_dir}/config/list-os | grep -x "$linuxos")" ]; then
+start0="bash ${script_dir}/bzu-gmb-${linuxos}-beta4.sh"
+echo "Linux OS: ${linuxos}"; else
+osnotsupported=1; fi
 
 #Топорная заглушка на случай если бета версия Ubuntu
 #echo $linuxos | grep "Ubuntu Focal Fossa" > /dev/null
@@ -92,19 +49,15 @@ done
 start=`echo $start0 | sed 's/ /-/2g'`
 #echo "start:" $start
 #exit 0
-
+if [ $osnotsupported = 1 ]; then
+echo "$linuxos is not supported OS"
+zenity --error --ellipsize --text="данная операционная система $linuxos не поддерживается bzu-gmb"
+exit 0
+fi
 #запуск скрипта согласно системы в которой он находиться
 #echo $start
 eval $start
 #заглушка на вывод ошибки если система не совместима со скриптом
-if [ $linuxos_status = 0 ];then
-linux=`echo $linuxos | sed 's/PRETTY_NAME=/ /g'`
-echo $linux "No support OS"
-zenity --error --ellipsize --text="данная операционная система $linux не поддерживается bzu-gmb"
-fi
-
-
-exit 0
 
 #Для создания скрипта использовались следующие ссылки
 #https://techblog.sdstudio.top/blog/google-drive-vstavliaem-priamuiu-ssylku-na-izobrazhenie-sayta
