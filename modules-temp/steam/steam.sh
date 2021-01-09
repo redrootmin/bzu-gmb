b73d3801fa49f9f13cf8fefba539ed432de2095f
XtraHigh <52545668+xtrahigh@users.noreply.github.com>
2021-01-10 03:55:03 +0700

    Update steam.sh

--- modules-temp/steam/steam.sh
+++ modules-temp/steam/steam.sh
@@ -18,7 +18,7 @@ user_run_script=`cat "${script_dir}/config/user"`
 date_install=`date`
 
 #даем информацию в терминал какой модуль установливается
-tput setaf 2; echo "Установка клиента steam для Linux [https://store.steampowered.com/]. Версия скрипта 1.0, автор: Яцына М.А."
+tput setaf 2; echo "Установка клиента Steam для Linux [https://store.steampowered.com/]. Версия скрипта 1.0, автор: Яцына М.А."
 tput sgr0
 
 #запуск основных команд модуля
@@ -26,15 +26,15 @@ sudo -S apt install -f -y --reinstall steam
 
 #формируем информацию о том что в итоге установили и показываем в терминал
 tput setaf 2
-sudo -S dpkg --list | echo "Установлена утилита:"`grep "${name_script}" | sed s/"ii"//g`
+sudo -S dpkg --list | echo "Установлена утилита "`grep "${name_script}" | sed s/"ii"//g`
 #сброс цвета текста в терминале
 tput sgr0
 
 #добавляем информацию в лог установки о уровне ошибок модуля, чем выше цифра, тем больше было ошибок и нужно проверить модуль разработчику
-echo "модуль ${name_script}, дата установки:${date_install}, количество ошибок:${error}"	 				  >> "${script_dir}/module_install_log"
+echo "Модуль ${name_script}, дата установки: ${date_install}, количество ошибок: ${error}"	 				  >> "${script_dir}/module_install_log"
 #Добавляем информацию о изменении флагов в файле настройки GRUB в лог установки
-echo "ВНИМАНИЕ: для запуска игр вам нужно обнавить steam до beta версии и велючить функцию steamplay"	 				  >> "${script_dir}/module_install_log"
-echo "сайт где можно посмотреть какие windows игры работаю в steam и в каком качестве:https://www.protondb.com/"	 				  >> "${script_dir}/module_install_log"
+echo "ВНИМАНИЕ: для запуска игр вам нужно обнавить Steam до beta версии и включить функцию Steam Play"	 				  >> "${script_dir}/module_install_log"
+echo "Сайт, где можно посмотреть, какие Windows игры работают в Steam и в каком качестве: https://www.protondb.com/"	 				  >> "${script_dir}/module_install_log"
 echo "Подробнее о командах и фукнциях Proton тут: https://github.com/ValveSoftware/Proton"	 				  >> "${script_dir}/module_install_log"
 
 exit 0
