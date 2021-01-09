d53151ff201f577bc69a02c4fa9815b63bbed809
XtraHigh <52545668+xtrahigh@users.noreply.github.com>
2021-01-10 03:47:22 +0700

    Update module-test1.sh

--- modules-temp/module-test1.sh
+++ modules-temp/module-test1.sh
@@ -7,10 +7,10 @@ script_dir=$(cd $(dirname "$0") && pwd);
 version=`cat "${script_dir}/config/name_version"`\
 date_install=`date`
 #даем информацию в терминал какой модуль установливается
-tput setaf 2; echo "Установка репозитария с актуальной версией драйвера Mesa, версия скрипта 1.0, автор: Яцына М.А."
+tput setaf 2; echo "Установка репозитория с актуальной версией драйвера Mesa, версия скрипта 1.0, автор: Яцына М.А."
 tput sgr0
 #проверяем установлена утилита inxi - информация о низкоуровневом ПО и железе
-dpkg -s inxi | grep installed > /dev/null || echo 'no install inxi :(' | sudo -S apt install -f -y inxi
+dpkg -s inxi | grep installed > /dev/null || echo 'inxi is not installed :(' | sudo -S apt install -f -y inxi
 inxistatus=`dpkg -s inxi | grep installed`
 echo "INXI" $inxistatus
 
@@ -23,9 +23,9 @@ yes Y | sudo -S apt-get autoremove  || error+=1
 yes Y | sudo -S apt-get clean  || error+=1
 #формируем информацию о том что в итоге установили и показываем в терминал
 mesa_version=`inxi -G | grep "Mesa"`  || error+=1
-tput setaf 2; echo "Установлен драйвер:${mesa_version}"  || error+=1
+tput setaf 2; echo "Установлен драйвер ${mesa_version}"  || error+=1
 #добавляем информацию в лог установки о уровне ошибок модуля, чем выше цифра, тем больше было ошибок и нужно проверить модуль разработчику
-echo "модуль ${script_name}, дата установки:${date_install}, количество ошибок:${error}"	 				  >> "${script_dir}/module_install_log"
+echo "Модуль ${script_name}, дата установки: ${date_install}, количество ошибок: ${error}"	 				  >> "${script_dir}/module_install_log"
 #сброс цвета текста в терминале
 tput sgr0
 #задержка вывода информации о итогах установки, что бы пользователь мог ознакомиться.
