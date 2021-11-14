ff522e21724e4dd86236e79da2bcdb04b259364a
redrootmin <redroot.min@gmail.com>
2021-11-14 09:25:16 +0300

    update debian

--- modules-temp/gdebi/gdebi.sh
+++ modules-temp/gdebi/gdebi.sh
@@ -18,7 +18,7 @@ script_dir_install=`cat "${script_dir}/config/script_dir_install"`
 date_install=`date`
 
 #даем информацию в терминал какой модуль установливается
-tput setaf 2; echo "Установка утилиты xboxdrv [https://gitlab.com/xboxdrv/xboxdrv]. Версия скрипта 1.0, автор: Яцына М.А."
+tput setaf 2; echo "Установка утилиты gdebi [https://launchpad.net/gdebi]. Версия скрипта 1.0, автор: Яцына М.А."
 tput sgr0
 
 #запуск основных команд модуля
@@ -26,7 +26,7 @@ sudo -S killall xboxdrv || true
 sudo -S rm -r "${script_dir}/modules-temp/${name_script}/temp" || let "error += 1"
 sudo -S rm -r "/usr/share/${name_script}" || true
 sudo -S rm "/usr/share/applications/${name_script}.desktop" || true
-sudo -S apt install -f -y --reinstall xboxdrv
+sudo -S apt install -f -y --reinstall gdebi gdebi-core
 sudo -S mkdir -p "${script_dir}/modules-temp/${name_script}/temp" || let "error += 1"
 cd "${script_dir}/modules-temp/${name_script}/temp"|| let "error += 1"
 sudo -S wget "https://drive.google.com/uc?export=download&id=12P9aCmSYgXbtWbXuIDP6bUcu5G0xT0wo" -O "${name_script}.tar.xz" || let "error += 1"
@@ -47,10 +47,5 @@ bash -c "${script_dir_install}${name_script}/${name_script}.sh" & sleep 5;sudo -
 
 #добавляем информацию в лог установки о уровне ошибок модуля, чем выше цифра, тем больше было ошибок и нужно проверить модуль разработчику
 echo "модуль ${name_script}, дата установки:${date_install}, количество ошибок:${error}"	 				  >> "${script_dir}/module_install_log"
-#Добавляем информацию о изменении флагов в файле настройки GRUB в лог установки
-echo "ВНИМАНИЕ: xboxdrv-qt-5.6-rc является неофицальным GUI для драйвера xboxdrv, поэтому разработчики рекомендуют пользоваться утилитой в консоле, пример: "	 				  >> "${script_dir}/module_install_log"
-echo "cat /proc/bus/input/devices | grep "ipega media gamepad controller" -A 4 | grep -oh "event[1-9]*[1-9]""	 				  >> "${script_dir}/module_install_log"
-echo "sudo xboxdrv --evdev /dev/input/event** --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_Z=x2,ABS_RZ=y2,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y --axismap -Y1=Y1,-Y2=Y2 --evdev-keymap BTN_A=a,BTN_B=b,BTN_X=x,BTN_Y=y,BTN_TL=lb,BTN_TR=rb,BTN_TL2=lt,BTN_TR2=rt,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start --silent &"	 				  >> "${script_dir}/module_install_log"
-echo "Подробнее о командах и фукнциях тут: https://xboxdrv.gitlab.io/xboxdrv.html"	 				  >> "${script_dir}/module_install_log"
 
 exit 0
