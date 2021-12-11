a65b2d4c7d5bf908f2221ce27c7e15fef8d447dc
redrootmin <redroot.min@gmail.com>
2021-12-12 01:37:26 +0300

    new modules

--- modules-temp/noisetorch-app/noisetorch-app.sh
+++ modules-temp/noisetorch-app/noisetorch-app.sh
@@ -26,7 +26,7 @@ version_app=${module_conf[7]}
 pass_user="$1"
 
 #даем информацию в терминал какой модуль устанавливается
-tput setaf 2; echo "Установка MagicaVoxel  - 3Д-редактора вокселей с поддержкой GPU и визуализацией трассировки лучей [https://ephtracy.github.io/]. Запуск программы осуществляется с помощью портативный версии wine от проекта Kron4ek [https://github.com/Kron4ek/Wine-Builds/releases]. Версия скрипта 1.0b, автор: Яцына М.А."
+tput setaf 2; echo "Установка NoiseTorch - виртуальный микрофон с автоматическим шумодавом на основе нейронной сеть RNNoise [https://github.com/lawl/NoiseTorch]. Версия скрипта 1.0b, автор: Яцына М.А."
 tput sgr0
 
 
@@ -37,32 +37,38 @@ then
 mkdir -p "/home/${user_run_script}/.local/share/applications"
 fi
 
-# Проверка установлен vscodium или нет в папке пользователя
-if [ ! -d "/home/${user_run_script}/magica-voxel-wine" ]
+# Проверка что существует папка .local/bin, если нет, создаем ее
+if [ ! -d "/home/${user_run_script}/.local/bin" ]
 then
-tput setaf 2; echo "3Д-редактор ${version_app} не установлен в папку пользователя ${user_run_script}, поэтому можно устанавливать :)"
+mkdir -p "/home/${user_run_script}/.local/bin"
+fi
+
+# Проверка установлен vscodium или нет в папке пользователя
+if [ -e "/home/${user_run_script}/.local/bin/noisetorch" ]
+then
+tput setaf 1; echo "Утилита ${name_script} уже установлена в папку пользователя ${user_run_script}, что бы не стереть ваши важные данные, установка прирывается!"
+tput sgr0
+else
+tput setaf 2; echo "Утилита ${name_script} не установлена в папку пользователя ${user_run_script}, поэтому можно устанавливать :)"
 tput sgr0
 cd
-name_app_folder="magica-voxel-wine"
-name_app_arhive="${name_app_folder}.tar.xz"
-name_script_start="magica-voxel_starter.sh"
+name_app_folder="noisetorch"
+name_app_arhive="noisetorch-app.tar.xz"
+name_script_start="noisetorch"
 rm -f ${name_app_arhive}
-wget https://github.com/redrootmin/bzu-gmb-modules/releases/download/v1/magica-voxel-wine.tar.xz
+wget https://github.com/redrootmin/bzu-gmb-modules/releases/download/v1/noisetorch-app.tar.xz
 tar -xpJf "${name_app_arhive}"
 rm -f ${name_app_arhive}
-cd ~/${name_app_folder};chmod +x mini_install.sh
-bash mini_install.sh
+#cd ~/${name_app_folder};chmod +x mini_install.sh
+#bash mini_install.sh
 
 # 5 секунд теста программы
 #app_name="MagicaVoxel.exe"
 #echo "Testing:${version_app}"
 #cd "/home/${user_run_script}/${name_app_folder}"
-echo "Папка установки:/home/${user_run_script}/${name_app_folder}"
+echo "Папка установки:/home/${user_run_script}/.local/bin/"
 #bash -c "/home/${user_run_script}/${name_app_folder}/app/${name_script_start}" & sleep 20;echo "${pass_user}" | sudo -S killall "${app_name}"
-tput setaf 2; echo "Установка утилиты ${version_app} завершена :)"
-tput sgr0
-else
-tput setaf 1; echo "Утилита ${version_app} уже установлен в папку пользователя ${user_run_script}, что бы не стереть ваши важные данные, установка прирывается!"
+tput setaf 2; echo "Установка утилиты ${name_script} завершена :)"
 tput sgr0
 fi
 
