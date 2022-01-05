6acc99aa818dd9fa18658b4dde2d47c0ef07982f
redrootmin <redroot.min@gmail.com>
2022-01-05 06:12:21 +0300

    fix bug

--- modules-temp/amd-anti-tearing/amd-anti-tearing.sh
+++ modules-temp/amd-anti-tearing/amd-anti-tearing.sh
@@ -32,17 +32,17 @@ tput setaf 2; echo "Данный скрипт задействует опцию 
 tput sgr0
 
 #объявляем нужные переменные для скрипта
-export dir_x11_file="/usr/share/X11/xorg.conf.d"
+export dir_x11_file="/etc/X11/xorg.conf.d"
 export x11_file_name="10-amdgpu.conf"
 readarray -t x11_commands_all < "${script_dir}/modules-temp/${name_script}/x11-commands"
-echo "$pass_user" | sudo -S chmod 777 "/usr/share/X11/xorg.conf.d/10-amdgpu.conf" || true
-echo "$pass_user" | sudo -S chmod -Rf 777 "/usr/share/X11/xorg.conf.d" || true
+echo "$pass_user" | sudo -S chmod 777 "${dir_x11_file}/${x11_file_name}" || true
+echo "$pass_user" | sudo -S chmod -Rf 777 "${dir_x11_file}" || true
 
 # Проверка что существует папка /usr/share/X11/xorg.conf.d, если нет, создаем ее
-if [ ! -d "/usr/share/X11/xorg.conf.d" ]
+if [ ! -d "${dir_x11_file}" ]
 then
-echo "$pass_user" | sudo -S mkdir -p "/usr/share/X11/xorg.conf.d"
-echo "$pass_user" | sudo -S chmod -Rf 777 "/usr/share/X11/xorg.conf.d"
+echo "$pass_user" | sudo -S mkdir -p "${dir_x11_file}"
+echo "$pass_user" | sudo -S chmod -Rf 777 "${dir_x11_file}"
 fi
 
 
@@ -50,9 +50,9 @@ fi
 # Проверка создан файл 10-amdgpu.conf или нет
 if [ -e "${dir_x11_file}/${x11_file_name}" ];then
 echo "${pass_user}" | sudo -S cp -p -f "${dir_x11_file}/${x11_file_name}" "${dir_x11_file}/${x11_file_name}.bak"
-tput setaf 2;echo "сделан бикап файла 10-amdgpu.conf в /usr/share/X11/xorg.conf.d/10-amdgpu.conf.bak"
+tput setaf 2;echo "сделан бикап файла ${x11_file_name} в ${dir_x11_file}/${x11_file_name}.bak"
 tput sgr0
-tput setaf 3; echo "Файл 10-amdgpu.conf уже создан, проверяем его на наличе опции: Option  "TearFree" "true""
+tput setaf 3; echo "Файл ${x11_file_name} уже создан, проверяем его на наличе опции: Option  "TearFree" "true""
 tput sgr0
 full_data_10_amdgpu=`cat ${dir_x11_file}/${x11_file_name}`
 if echo "${full_data_10_amdgpu}" | grep -ow 'Option  "TearFree" "true"' > /dev/null;then
@@ -62,7 +62,7 @@ tput setaf 2
 echo "$pass_user" | sudo -S cat ${dir_x11_file}/${x11_file_name}
 tput sgr0
 else
-tput setaf 1; echo "В файле 10-amdgpu.conf нет опции: Option  "TearFree" "true", поэтому создаем его заново"
+tput setaf 1; echo "В файле ${x11_file_name} нет опции: Option  "TearFree" "true", поэтому создаем его заново"
 tput sgr0
 #задем переменной колличество пакетов в массиве
 packages_number=${#x11_commands_all[@]}
@@ -78,11 +78,11 @@ echo "$pass_user" | sudo -S echo "${x11_commands_all[$i]}" >> "${dir_x11_file}/$
 i=$(($i + 1))
 done
 fi
-tput setaf 2;echo "файл создан:"
+tput setaf 2;echo "файл ${x11_file_name} создан:"
 echo "$pass_user" | sudo -S cat ${dir_x11_file}/${x11_file_name}
 tput sgr0
 else
-tput setaf 1; echo "Файл 10-amdgpu.conf не существует, поэтому создаем его"
+tput setaf 1; echo "Файл ${x11_file_name}  не существует, поэтому создаем его"
 tput sgr0
 #задем переменной колличество пакетов в массиве
 packages_number=${#x11_commands_all[@]}
@@ -97,12 +97,12 @@ do
 echo "$pass_user" | sudo -S echo "${x11_commands_all[$i]}" >> "${dir_x11_file}/${x11_file_name}"
 i=$(($i + 1))
 done
-tput setaf 2;echo "файл создан:"
+tput setaf 2;echo "файл ${x11_file_name} создан:"
 echo "$pass_user" | sudo -S cat ${dir_x11_file}/${x11_file_name}
 tput sgr0
 fi
-echo "$pass_user" | sudo -S chmod 744 /usr/share/X11/xorg.conf.d/10-amdgpu.conf || true
-echo "$pass_user" | sudo -S chmod -Rf 755 "/usr/share/X11/xorg.conf.d" || true
+echo "$pass_user" | sudo -S chmod 744 "${dir_x11_file}/${x11_file_name}" || true
+echo "$pass_user" | sudo -S chmod -Rf 755 "${dir_x11_file}" || true
 #добавляем информацию в лог установки о уровне ошибок модуля, чем выше цифра, тем больше было ошибок и нужно проверить модуль разработчику
 echo "модуль ${name_script}, дата установки:${date_install}, количество ошибок:${error}"	 				  >> "${script_dir}/module_install_log"
 #echo "Подробнее на [https://unix.stackexchange.com/questions/554908/disable-spectre-and-meltdown-mitigations/565516#565516]"	 				  >> "${script_dir}/module_install_log"
