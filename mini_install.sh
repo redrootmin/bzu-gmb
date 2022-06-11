238cf8bf80303994de9c806ad78b32160ed992ed
redrootmin <redroot.min@gmail.com>
2022-06-11 19:01:35 +0300

    update install

--- mini_install.sh
+++ mini_install.sh
@@ -194,6 +194,7 @@ echo "${pass_user}" | sudo -S dnf remove -y gnome-robots four-in-a-row gnuchess 
  fi
 echo "${pass_user}" | sudo -S dnf install -y inxi xow libusb-compat0.1_4 paprefs pavucontrol ananicy p7zip python3 zenity yad grub-customizer libfuse2-devel libfuse3-devel libssl1.1 neofetch git meson ninja gcc gcc-c++ cmake.i686 cmake glibc-devel dbus-devel glslang vulkan.x86_64 vulkan.i686 lib64vulkan-devel.x86_64 lib64vulkan-intel-devel.x86_64 lib64vulkan1.x86_64 libvulkan-devel.i686 libvulkan-intel-devel.i686 libvulkan1.i686
 echo "${pass_user}" | sudo -S dnf autoremove -y
+echo "${pass_user}" | sudo -S dnf clean packages
 ##################################################################################
 #загружаем список пакетов из файла в массив
 readarray -t packages_list < "${script_dir}/config/packages-rosa"
